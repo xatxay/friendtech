1fe15c79959c4be510d323017ebac515dfefc6b7
xatxay <xatxaysmk@gmail.com>
2023-10-03 16:06:46 -0500

    get individual object change

--- src/FTScrape.ts
+++ src/FTScrape.ts
@@ -7,15 +7,38 @@ async function getTradeData() {
     const headers = {
       Authorization: loginToken,
     };
+    console.log('fetching');
     const response = await axios.get(process.env.API, { headers });
-    return response.data.events;
+    return response.data;
   } catch (err) {
     console.error(err);
     return '';
   }
 }
 
-(async () => {
-  const data = await getTradeData();
-  console.log(data);
-})();
+async function setCronjob() {
+  let oldData = '';
+  setInterval(async () => {
+    try {
+      const data = await getTradeData();
+      const newData = data.events;
+      const newDataString = JSON.stringify(newData);
+      console.log('monitoring@@@@@@@@@@@@@@@@@@@@@@@@@');
+      for (const trade of newData) {
+        const tradeString = JSON.stringify(trade);
+        if (!oldData.includes(tradeString)) {
+          console.log(
+            `--------------New Trade-----------\n ${trade.trader.name} ${trade.isBuy ? 'bought' : 'sold'} ${
+              trade.subject.name
+            } key|ETH: ${(trade.ethAmount / 1000000000000000000).toFixed(2)} ${new Date(trade.createdAt)}`,
+          );
+        }
+        oldData = newDataString;
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }, 10000);
+}
+
+setCronjob();
